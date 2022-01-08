768161a990818069285dcb605828006e16efc5e3
JLCraw77 <94502047+JLCraw77@users.noreply.github.com>
2022-01-08 14:16:21 -0500

    adding PyPoll.py file

---
+++ Activity 1.py
@@ -0,0 +1,16 @@
+name="Jason"
+Country="United States"
+age=32
+hourly_wage=120
+satisfied=True
+
+daily_wage=hourly_wage*8
+
+print(f"My Name is {name}")
+print(f"My Country is {Country}")
+print(f"My age = {age}")
+print(f"My Hourly wage is {hourly_wage}")
+
+print(f"The Daily Wage is {daily_wage}")
+print(f"Are you satified with the result {satisfied}")
+
---
+++ Python_Practice.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+counties = ("Arapahoe","Denver","Jefferson")
+counties[1]=='Denver'
+print(counties[1])
+
+temperature=int(input("What is the temperature outside? "))
+
+if temperature >80:
+    print("turn on the AC")
+else:
+    print("open the windows")
